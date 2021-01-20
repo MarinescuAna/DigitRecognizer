f3e5a1fff3b6c23c65cc381ad45f39a71fef3437
marinescuana11 <marinescuana28@gmail.com>
2021-01-20 15:32:56 +0200

    gui

--- GUI.py
+++ GUI.py
@@ -8,15 +8,11 @@ import numpy as np
 model = load_model('mnist.h5')
 
 def predict_digit(img):
-    #resize image to 28x28 pixels
     img = img.resize((28,28))
-    #convert rgb to grayscale
     img = img.convert('L')
     img = np.array(img)
-    #reshaping to support our model input and normalizing
     img = img.reshape(1,28,28,1).astype('float32')
     img = img/255.0
-    #predicting the class
     res = model.predict([img])[0]
     return np.argmax(res), max(res)
 
@@ -26,27 +22,24 @@ class App(tk.Tk):
 
         self.x = self.y = 0
         
-        # Creating elements
         self.canvas = tk.Canvas(self, width=500, height=500, bg = "white", cursor="cross")
         self.label = tk.Label(self, text="Draw..", font=("Helvetica", 48))
         self.classify_btn = tk.Button(self, text = "Recognise", command = self.classify_handwriting)   
         self.button_clear = tk.Button(self, text = "Clear", command = self.clear_all)
        
-        # Grid structure
         self.canvas.grid(row=0, column=0, pady=2, sticky=W, )
         self.label.grid(row=0, column=1,pady=2, padx=2)
         self.classify_btn.grid(row=1, column=1, pady=2, padx=2)
         self.button_clear.grid(row=1, column=0, pady=2)
         
-        #self.canvas.bind("<Motion>", self.start_pos)
         self.canvas.bind("<B1-Motion>", self.draw_lines)
 
     def clear_all(self):
         self.canvas.delete("all")
         
     def classify_handwriting(self):
-        HWND = self.canvas.winfo_id()  # get the handle of the canvas
-        rect = win32gui.GetWindowRect(HWND)  # get the coordinate of the canvas
+        HWND = self.canvas.winfo_id()
+        rect = win32gui.GetWindowRect(HWND) 
         a,b,c,d = rect
         rect=(a+4,b+4,c-4,d-4)
         im = ImageGrab.grab(rect)
@@ -57,7 +50,7 @@ class App(tk.Tk):
     def draw_lines(self, event):
         self.x = event.x
         self.y = event.y
-        r=12
+        r=14
         self.canvas.create_oval(self.x-r, self.y-r, self.x + r, self.y + r, fill='black')
        
 app = App()
